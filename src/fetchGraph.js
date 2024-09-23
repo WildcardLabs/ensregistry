b0c6ca5d5633e0c72a8e521eddf8b3f1d3e9a3c3
officialcmg <70940749+officialcmg@users.noreply.github.com>
2024-09-24 00:23:11 +0300

    modified fetching logic

--- src/fetchGraph.js
+++ src/fetchGraph.js
@@ -3,7 +3,7 @@ import { gql, request } from 'graphql-request'
 
 const url = 'https://api.studio.thegraph.com/query/89559/ensregistry/v0.0.4'
 const query = gql`{
-  nameRegistereds(first: 50, orderBy: blockNumber, orderDirection: desc) {
+  nameRegistereds(first: 60, orderBy: blockNumber, orderDirection: desc) {
     id
     name
     owner
